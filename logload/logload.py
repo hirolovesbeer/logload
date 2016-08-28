4e89378bb77547fcb7c7f3e5075deebf5444bb16
Mark Meyer <mark@ofosos.org>
2016-08-28 12:17:47 +0200

    Expand parsing and tests

--- logload/logload.py
+++ logload/logload.py
@@ -1,39 +1,6 @@
 
-import random
-
-### tokenizer
-
-from re import VERBOSE
-
-from funcparserlib.lexer import make_tokenizer, Token, LexerError
-
-regexps = {
-    'escaped': r'''
-        \\                                  # Escape
-          ((?P<standard>["\\/bfnrt])        # Standard escapes
-        | (u(?P<unicode>[0-9A-Fa-f]{4})))   # uXXXX
-        ''',
-    'unescaped': r'''
-        [^"\\]                              # Unescaped: avoid ["\\]
-        ''',
-}
-
-def tokenize(str):
-    """ str -> Sequence(Token) """
-    specs = [
-        ('Space', (r'[ \t\r\n]+', )),
-        ('String', (r'"(%(unescaped)s | %(escaped)s)*"' % regexps, VERBOSE)),
-        ('Op', (r'[\[\],]', )),
-        ('Name', (r'[A-Za-z_][A-Za-z_0-9]*',)),
-    ]
-    empty = ['Space']
-    t = make_tokenizer(specs)
-    return [x for x in t(str) if x.type not in empty]
-
-### parser
-
-
 ### generator
+import random
 
 class Fragment:
     def expand(self):
@@ -57,16 +24,115 @@ class StaticSelection(Fragment):
 
     def expand(self):
         return s
+
+### tokenizer
+
+from re import VERBOSE
+
+from funcparserlib.lexer import make_tokenizer, Token, LexerError
+
+regexps = {
+    'escaped': r'''
+        \\                                  # Escape
+          ((?P<standard>["\\/bfnrt])        # Standard escapes
+        | (u(?P<unicode>[0-9A-Fa-f]{4})))   # uXXXX
+        ''',
+    'unescaped': r'''
+        [^"\\]                              # Unescaped: avoid ["\\]
+        ''',
+}
+
+def tokenize(str):
+    """ str -> Sequence(Token) """
+    specs = [
+        ('Space', (r'[ \t\r\n]+', )),
+        ('String', (r'"(%(unescaped)s | %(escaped)s)*"' % regexps, VERBOSE)),
+        ('Op', (r'[\[\],()]', )),
+        ('Name', (r'[A-Za-z_][A-Za-z_0-9]*',)),
+    ]
+    empty = ['Space']
+    t = make_tokenizer(specs)
+    return [x for x in t(str) if x.type not in empty]
+
+### parser
+
+import re
+
+re_esc = re.compile(regexps[u'escaped'], VERBOSE)
+
+from funcparserlib.parser import (some, a, maybe, many, finished, skip,
+                                  forward_decl, NoParseError)
+
+def parse(seq):
+    tokval = lambda x: x.value
+    toktype = lambda t: some(lambda x: x.type == t) >> tokval
+    op = lambda s: a(Token('Op', s)) >> tokval
+    op_ = lambda s: skip(op(s))
+
+    def make_group(n):
+        if n is None:
+            return ['grp']
+        else:
+            return ['grp', n[0]] + n[1]
+
+    def make_list(n):
+        if n is None:
+            return ['lis']
+        else:
+            return ['lis', n[0]] + n[1]
+        
+    def unescape(s):
+        std = {
+            u'"': u'"', u'\\': u'\\', u'/': u'/', u'b': u'\b', u'f': u'\f',
+            u'n': u'\n', u'r': u'\r', u't': u'\t',
+        }
+
+        def sub(m):
+            if m.group('standard') is not None:
+                return std[m.group('standard')]
+            else:
+                return unichr(int(m.group('unicode'), 16))
+
+        return re_esc.sub(sub, s)
+
+    def make_name(n):
+        return n
+    
+    def make_string(n):
+        return unescape(n[1:-1])
+
+    name = toktype('Name') >> make_name
+    string = toktype('String') >> make_string
+    value = forward_decl()
+
+    group = (
+        op_('[') +
+        maybe(value + many(op_(',') + value)) +
+        op_(']')
+        >> make_group)
+
+    lis = (
+        op_('(') +
+        maybe(value + many(op_(',') + value)) +
+        op_(')')
+        >> make_list)
+
+    value.define(string | name)
+    
+    logload = lis | group
+    logload_text = logload + skip(finished)
+
+    return logload_text.parse(seq)
+
+### main & options
     
 class LogLineGenerator:
 
     def set_pattern(pat):
-        pass
+        self.spec = parse(tokenize(pat))
 
     def lines(i):
         pass
-
-### main & options
     
 def main():
     pass
--- test/parsing.py
+++ test/parsing.py
@@ -12,6 +12,25 @@ class TestTokenizer(unittest.TestCase):
     def test_group(self):
         logload.tokenize("[ \"foo\", \"bar\" ]")
 
+    def test_list(self):
+        logload.tokenize("( \"foo\", glorg )")
 
+    def test_empty(self):
+        logload.tokenize("")
+
+class TestParser(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_empty(self):
+        logload.parse(logload.tokenize(""))
+
+    def test_empty_list(self):
+        logload.parse(logload.tokenize("( )"))
+        
+    def test_list_string(self):
+        logload.parse(logload.tokenize("( \"foobar\" )"))
+
+    def test_empty_group(self):
+        logload.parse(logload.tokenize("[ \"foobar\", rand ]"))
+        
 if __name__ == "__main__":
     unittest.main()
